39db4345e392791af2b9acbc2bf9c8f1b16898d4
USUARIO <USUARIO@DESKTOP-VQD7OE1>
2025-04-17 15:23:44 -0500

    Cargar linea base
    Cargar arquetipo inicial de la linea base para comenzar a evolucionarlo.

---
+++ smartpark-ms/src/main/java/uco/edu/co/smartpark/initializer/SmartparkMsApplication.java
@@ -0,0 +1,16 @@
+package uco.edu.co.smartpark.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartparkMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartparkMsApplication.class, args);
+	}
+
+}
+
+
+
---
+++ smartpark-ms/src/test/java/co/edu/co/smartpark/initializer/SmartparkMsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.co.smartpark.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartparkMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
